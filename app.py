e51987145bb519f79e1df20d022bbd6be2b6bb69
Rick Tesmond <usrfct@tesmond.workpc.tds.net>
2020-03-04 13:48:09 -0600

    working api, work on ui

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from connexion.resolver import RestyResolver
-import connexion
+from flask import Flask, request, jsonify
+from word_pyramid.word_pyramid import process_word
+
+app = Flask(__name__)
+
+
+@app.route('/pyramid-word', methods=['GET'])
+def word_handler():
+    try:
+        req = request.args.get('word')
+        if req:
+            resp = process_word(req)
+            return resp
+        else:
+            #raise 400 error for improper data format
+            return jsonify(
+                response="Data passed incorrectly. Ensure you entered using the format '/pyramid-word?word=<your word>'"
+            )
+    except Exception as e:
+        return e
 
 
 if __name__ == '__main__':
-    app = connexion.App(__name__, port=9090, specification_dir='spec/')
-    app.add_api('word_pyramid_api.yaml', resolver=RestyResolver('word_pyramid'))
-    app.run()
+    app.run(debug=True, port=9090)
--- word_pyramid/word_pyramid.py
+++ word_pyramid/word_pyramid.py
@@ -1,19 +1,32 @@
-from collections import Counter, OrderedDict
+from collections import Counter
+from flask import jsonify
 
 
-def get(pyramid_word):
-	# import pdb;pdb.set_trace()
-	_letter_data = list(Counter(pyramid_word).items())
-	ordered_letters = sorted(_letter_data, key=lambda x: x[1])
-	for i in range(len(ordered_letters)-1):
-		try:
-			if ordered_letters[i][1]+1 == ordered_letters[i+1][1]:
-				continue
-			else:
-				return "The word passed is not a pyramid word: '{}'. Word Output: {}".format(pyramid_word, ordered_letters)
-		except KeyError:
-			return "PYRAMID FOUND: The word passed is a pyramid word: '{}'. Word Output: {}".format(pyramid_word, ordered_letters)
+def process_word(pyramid_word):
+	if pyramid_word.isalpha():
+		_letter_data = list(Counter(pyramid_word.lower()).items())
+		ordered_letters = sorted(_letter_data, key=lambda x: x[1])
+		for i in range(len(ordered_letters)):
+			try:
+				if ordered_letters[i][1]+1 == ordered_letters[i+1][1]:
+					continue
+				else:
+					return build_resp(
+						"NOT A PYRAMID: The word passed is not a pyramid word:'{}'. Word pyramid output: {}".format(
+							pyramid_word, ordered_letters), 200
+					)
+			except IndexError:
+				return build_resp("PYRAMID FOUND: The word passed is a pyramid word: '{}'. Word pyramid output: {}".format(
+					pyramid_word, ordered_letters), 200
+				)
+	else:
+		return build_resp(
+			"Unable to process: Word contains numbers or special chars. Word {}".format(pyramid_word), 400
+		)
 
 
-	
-	
+def build_resp(resp_data, status_code):
+	return jsonify(
+		response=resp_data,
+		status=status_code
+	)
